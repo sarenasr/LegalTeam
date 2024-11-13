ad00be2cb17c5372e1a18f3266a05080e52ad779
s.arensr <s.arenasr@uniandes.edu.co>
2024-11-13 18:31:16 -0500

    starting scraping

--- LegalTeam/settings.py
+++ LegalTeam/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    "scraper.apps.ScraperConfig",
+    "analysis.apps.AnalysisConfig",
+    "notifications.apps.NotificationsConfig"
 ]
 
 MIDDLEWARE = [
