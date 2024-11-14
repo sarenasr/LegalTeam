14c288ca8049a302530b3e096ba3cd7da5e5bea7
s.arensr <s.arenasr@uniandes.edu.co>
2024-11-13 19:19:37 -0500

    Fixing startup errors intial commit new

--- LegalTeam/scraper/forms.py
+++
@@ -1,9 +0,0 @@
-# scraper/forms.py
-from django import forms
-
-class CaseForm(forms.Form):
-    case_number = forms.CharField(
-        label="Número de Radicado",
-        max_length=23,
-        widget=forms.TextInput(attrs={'placeholder': 'Ingrese los 23 dígitos del número de Radicación'})
-    )
--- LegalTeam/settings.py
+++ LegalTeam/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 
+from LegalTeam import notifications
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -38,8 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     "scraper.apps.ScraperConfig",
-    "analysis.apps.AnalysisConfig",
-    "notifications.apps.NotificationsConfig"
+    "notifications.apps.NotificationsConfig",
+    "analysis.apps.AnalysisConfig"
 ]
 
 MIDDLEWARE = [
