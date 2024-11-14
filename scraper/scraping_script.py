a38397cf883c0e7c6fec809ae0d707d922110607
s.arensr <s.arenasr@uniandes.edu.co>
2024-11-13 21:04:53 -0500

    no window scraper

--- scraper/scraping_script.py
+++ scraper/scraping_script.py
@@ -18,9 +18,9 @@ def webscrape(num) -> None:
     # chrome_options.add_argument("--disable-extensions")
     # chrome_options.add_argument("--disable-gpu")
     # chrome_options.add_argument("--no-sandbox") # linux only
-    #chrome_options.add_argument("--headless=new") # for Chrome >= 109
+    chrome_options.add_argument("--headless=new") # for Chrome >= 109
     # chrome_options.add_argument("--headless")
-    chrome_options.headless = True # also works
+    #chrome_options.headless = True # also works
     chrome_options.add_experimental_option('excludeSwitches', ['enable-logging'])
     chrome_options.add_argument('--disable-dev-shm-usage')
     chrome_options.add_argument('--no-sandbox')
